c017e762460e4dbbe69e3f9d1c8a94748c2ea6ea
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-06-06 12:48:46 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        int ans = -1;
+
+        if(x == 0){
+            return 0;
+        }
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            long square = (long) mid * mid;
+
+            if(square == x){
+                return mid;
+            }else if(square > x){
+                end = mid - 1;
+            }else{
+                ans = mid; // could be a potential answer
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
