569009535da700ebbdc863bea45fe864556d668e
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-06-08 12:56:36 +0530

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p== null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+
+        boolean leftTree = isSameTree(p.left, q.left);
+        boolean rightTree = isSameTree(p.right, q.right);
+
+        return (leftTree && rightTree);
+    }
+}
+
+// Symmetric Tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return isSameTree(root.left, root.right);
+//     }
+
+//     public boolean isSameTree(TreeNode p, TreeNode q){
+//         if(p == null && q == null){
+//             return true; // both trees are empty
+//         }
+
+//         if((p == null && q != null) 
+//             || (p != null && q == null)
+//             || (p.val != q.val)){
+//                 return false; // trees are not same;
+//         } 
+        
+//         boolean check = true;
+//         check = (isSameTree(p.left, q.right) && isSameTree(p.right, q.left));
+//         return check;
+//     }
+// }
