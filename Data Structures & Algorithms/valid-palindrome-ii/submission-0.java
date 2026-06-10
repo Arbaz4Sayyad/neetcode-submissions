d2b852c6872ff244b979849e6224134be7de05d7
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-06-10 12:45:48 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                // we can delete only one character
+                return palindromeHelper(i+1, j, s) || palindromeHelper(i, j-1, s);
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    public boolean palindromeHelper(int i, int j, String s){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
