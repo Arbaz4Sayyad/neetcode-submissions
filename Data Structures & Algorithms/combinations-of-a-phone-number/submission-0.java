6e79da5fc4b4ee11e19ed974e008cda9bc1c9266
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-06-18 15:45:05 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        List<String> output = new ArrayList<>();
+
+        // handle empty input
+        if(digits.length() == 0){
+            return output;
+        }
+
+        backtrack(0, digits, "", map,  output);
+
+        return output;
+    }
+
+    public void backtrack(int index, String digits, String current, Map<Character, String> map, List<String> output){
+        // base case
+        if(index == digits.length()){
+            output.add(current);
+            return;
+        }
+
+        char digit = digits.charAt(index);
+        String letters = map.get(digit);
+        
+        for(int i = 0; i < letters.length(); i++){
+            // include 
+            // current += letters.charAt(i);
+            // backtrack(index + 1, digits, current, map,  output);
+            // current = current.substring(0, current.length() - 1);
+
+            backtrack(index + 1, digits, current + letters.charAt(i), map,  output);
+        }
+    }
+
+}
