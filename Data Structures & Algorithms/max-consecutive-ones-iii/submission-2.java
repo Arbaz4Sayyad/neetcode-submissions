b465b4711b37703fc61de3ef203605db2882d150
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-07-15 13:08:40 +0530

    Add: max-consecutive-ones-iii - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-2.java
@@ -0,0 +1,76 @@
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int n = nums.length;
+//         int maxLen = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             int zeroCount = 0;
+
+//             for(int j = i; j < n; j++) {
+//                 if(nums[j] == 0) {
+//                     zeroCount++;
+//                 }
+
+//                 if(zeroCount > k) {
+//                     break;
+//                 }
+
+//                 maxLen = Math.max(maxLen, j - i + 1);
+//             }
+//         }
+
+//         return maxLen;
+//     }
+// }
+
+
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int n = nums.length;
+//         int maxLen = 0;
+//         int zeroCount = 0;
+//         int left = 0;
+
+//         for(int right = 0; right < n; right++) {
+
+//             if(nums[right] == 0) {
+//                 zeroCount++;
+//             }
+
+//             while(zeroCount > k) {
+//                 if(nums[left] == 0) {
+//                     zeroCount--;
+//                 }
+
+//                 left++;
+//             }
+//             maxLen = Math.max(maxLen, right - left + 1);
+//         }
+//         return maxLen;
+//     }
+// }
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int maxLen = 0;
+        int zeroCount = 0;
+        int left = 0;
+
+        for(int right = 0; right < n; right++) {
+
+            if(nums[right] == 0) {
+                zeroCount++;
+            }
+
+            if(zeroCount > k) {
+                if(nums[left] == 0) {
+                    zeroCount--;
+                }
+
+                left++;
+            }
+        }
+        return n - left;
+    }
+}
