e03f9f537f9652b38328de201b8072213535856e
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-07-17 11:49:16 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.java
@@ -0,0 +1,66 @@
+// class Solution {
+//     public List<List<Integer>> combine(int n, int k) {
+//         List<List<Integer>> res =  new ArrayList<>();
+//         List<Integer> current = new ArrayList<>();
+//         backtrack(1, n, k, current, res);
+
+//         return res;
+//     }
+
+//     private void backtrack(int start, int n, int k, List<Integer> current, List<List<Integer>> res) {
+//         // Base case:
+//         // If current combination has k elements, store it.
+//         if (current.size() == k) {
+//             res.add(new ArrayList<>(current));
+//             return;
+//         }
+
+//         // Try every possible number from 'start' to n
+//         for (int i = start; i <= n; i++) {
+
+//             // Choose
+//             current.add(i);
+
+//             // Explore further with next number
+//             backtrack(i + 1, n, k, current, res);
+
+//             // Undo the choice (Backtrack)
+//             current.remove(current.size() - 1);
+//         }
+//     }
+// } 
+
+
+// Optimized Version (Pruning)
+// We can avoid unnecessary recursive calls by limiting the loop.
+
+class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backtrack(1, n, k, current, res);
+        return res;
+    }
+
+    private void backtrack(int start, int n, int k, List<Integer> current, List<List<Integer>> res) {
+
+        if (current.size() == k) {
+            res.add(new ArrayList<>(current));
+            return;
+        }
+
+        // Number of elements still needed
+        int remaining = k - current.size();
+
+        // Stop early if there aren't enough numbers left
+        for (int i = start; i <= n - remaining + 1; i++) {
+
+            current.add(i);
+
+            backtrack(i + 1, n, k, current, res);
+
+            current.remove(current.size() - 1);
+        }
+    }
+}
