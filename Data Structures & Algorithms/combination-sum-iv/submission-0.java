86195ada50d7883e3ff327767474089d4f003d4e
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-07-18 12:57:26 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.java
@@ -0,0 +1,58 @@
+// class Solution {
+//     public int combinationSum4(int[] nums, int target) {
+//         int n = nums.length;
+//         return findTotalCombiantions(n, nums, target);
+//     }
+
+//     private int findTotalCombiantions(int n, int[] nums, int target) {
+//         if(target == 0) {
+//             return 1;
+//         }
+
+//         if(target < 0) {
+//             return 0;
+//         } 
+
+//         int result = 0;
+
+//         for(int j = 0; j < n; j++) {
+//             result += findTotalCombiantions(n, nums, target - nums[j]);
+//         }
+
+//         return result;
+//     }
+// }
+
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[target+1];
+        Arrays.fill(dp, -1);
+
+        return findTotalCombiantions(n, nums, target, dp);
+    }
+
+    private int findTotalCombiantions(int n, int[] nums, int target, int[] dp) {
+        if(target == 0) {
+            return 1;
+        }
+
+        if(target < 0) {
+            return 0;
+        } 
+        if(dp[target] != -1) {
+            return dp[target];
+        }
+
+        int result = 0;
+
+        for(int j = 0; j < n; j++) {
+            result += findTotalCombiantions(n, nums, target - nums[j], dp);
+        }
+
+        dp[target] = result;
+
+        return dp[target];
+    }
+}
