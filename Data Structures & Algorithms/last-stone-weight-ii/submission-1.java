f178aecb746403a24ff68339d12d54fc1782642d
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-07-24 12:54:10 +0530

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.java
@@ -0,0 +1,139 @@
+// class Solution {
+
+//     public int lastStoneWeightII(int[] stones) {
+
+//         int totalSum = 0;
+//         for (int stone : stones) {
+//             totalSum += stone;
+//         }
+
+//         int target = totalSum / 2;
+
+//         // Maximum subset sum <= target
+//         int subsetSum = solve(0, target, stones);
+
+//         return totalSum - 2 * subsetSum;
+//     }
+
+//     // Returns maximum achievable subset sum
+//     private int solve(int index, int remaining, int[] stones) {
+
+//         // No stones left
+//         if (index == stones.length) {
+//             return 0;
+//         }
+
+//         // Option 1: Skip current stone
+//         int notTake = solve(index + 1, remaining, stones);
+
+//         // Option 2: Take current stone (if possible)
+//         int take = 0;
+
+//         if (stones[index] <= remaining) {
+//             take = stones[index] +
+//                     solve(index + 1,
+//                           remaining - stones[index],
+//                           stones);
+//         }
+
+//         return Math.max(take, notTake);
+//     }
+// }
+
+// class Solution {
+
+//     int[][] dp;
+
+//     public int lastStoneWeightII(int[] stones) {
+
+//         int totalSum = 0;
+
+//         for (int stone : stones) {
+//             totalSum += stone;
+//         }
+
+//         int target = totalSum / 2;
+
+//         dp = new int[stones.length][target + 1];
+
+//         // -1 means not computed
+//         for (int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+
+//         int subsetSum = solve(0, target, stones);
+
+//         return totalSum - 2 * subsetSum;
+//     }
+
+//     private int solve(int index, int remaining, int[] stones) {
+
+//         // No stones left
+//         if (index == stones.length) {
+//             return 0;
+//         }
+
+//         // Already computed
+//         if (dp[index][remaining] != -1) {
+//             return dp[index][remaining];
+//         }
+
+//         // Skip current stone
+//         int notTake = solve(index + 1, remaining, stones);
+
+//         // Take current stone
+//         int take = 0;
+
+//         if (stones[index] <= remaining) {
+//             take = stones[index]
+//                     + solve(index + 1,
+//                             remaining - stones[index],
+//                             stones);
+//         }
+
+//         return dp[index][remaining] = Math.max(take, notTake);
+//     }
+// }
+
+
+class Solution {
+
+    public int lastStoneWeightII(int[] stones) {
+
+        int totalSum = 0;
+
+        for (int stone : stones) {
+            totalSum += stone;
+        }
+
+        int target = totalSum / 2;
+        int n = stones.length;
+
+        // dp[i][j] = maximum subset sum using first i stones
+        int[][] dp = new int[n + 1][target + 1];
+
+        // Build DP table
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j <= target; j++) {
+
+                // Skip current stone
+                dp[i][j] = dp[i - 1][j];
+
+                // Take current stone if possible
+                if (stones[i - 1] <= j) {
+
+                    dp[i][j] = Math.max(
+                            dp[i][j],
+                            stones[i - 1] +
+                            dp[i - 1][j - stones[i - 1]]
+                    );
+                }
+            }
+        }
+
+        int subsetSum = dp[n][target];
+
+        return totalSum - 2 * subsetSum;
+    }
+}
