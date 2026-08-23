39757744d27eb80b9b010691788482bbe81601d2
Arbaz Sayyad <135498634+Arbaz4Sayyad@users.noreply.github.com>
2026-08-23 12:54:11 +0530

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+        // Stores:
+        // prefixSum -> frequency
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Important:
+        // Prefix sum 0 has appeared once before
+        // the array starts.
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            // Calculate prefix sum
+            currentSum += num;
+
+            // We need a previous prefix sum such that:
+            //
+            // currentSum - previousSum = k
+            //
+            // Therefore:
+            // previousSum = currentSum - k
+            int requiredSum = currentSum - k;
+
+            // If this prefix sum existed before,
+            // every occurrence gives us one valid subarray.
+            if (map.containsKey(requiredSum)) {
+                count += map.get(requiredSum);
+            }
+
+            // Store/update the frequency of current prefix sum
+            map.put(
+                currentSum,
+                map.getOrDefault(currentSum, 0) + 1
+            );
+        }
+
+        return count;
+    }
+}
+
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+
+//         int n = nums.length;
+//         int count = 0;
+
+//         // Choose the starting index
+//         for (int i = 0; i < n; i++) {
+
+//             int sum = 0;
+
+//             // Extend the subarray from i to j
+//             for (int j = i; j < n; j++) {
+
+//                 // Add the current element
+//                 sum += nums[j];
+
+//                 // Check if current subarray sum is k
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// }
